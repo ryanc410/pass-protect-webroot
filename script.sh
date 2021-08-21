2d0c6abe163e201ea2a04805350a4292651ef5ac
Ryan Cook <80371197+ryanc410@users.noreply.github.com>
2021-08-21 18:20:55 -0500

    08/21/2021
    Modified script. Need to add help menu

--- script.sh
+++ script.sh
@@ -1,46 +1,76 @@
 #!/bin/bash
+#
+# Author: Ryan
+# https://github.com/ryanc410
+# Date: Modified on 08/21/2021
 
-DOMAIN=$(hostname -f)
+# VARIABLES - fill in these variables before running the script
+########################
+
+DOMAIN=
+USERNAME=
+USERPASS=
+WEBROOT=
+
+# DONT CHANGE
+
 APACHE_LOG_DIR=/var/log/apache2
 
-check_installed() {
-    command -v htpasswd &>/dev/null
-        if [[ $? -ne 0 ]]; then
-            apt install apache2-utils -y
-        fi
-    which apache2 &>/dev/null
-        if [[ $? -ne 0 ]]; then
-            apt install apache2 -y
-            systemctl enable apache2 && systemctl start apache2
-        fi
+# FUNCTIONS
+########################
+
+checkroot()
+{
+    if [[ $EUID -ne 0 ]]; then
+        clear
+        echo "Must be root to run this script!"
+        sleep 2
+        exit 1
+    fi
 }
-gather_info() {
+
+header()
+{
     clear
-    echo "############################################"
+    echo "#############################################"
     echo "#    Pass-Protect Web Directories Script    #"
     echo "#############################################"
     echo ""
-    echo "Enter your name:"
-    read username
-    echo ""
-    echo "Enter a password:"
-    read userpass
-    echo ""
-    echo "Enter the path to your web root directory:"
-    read webroot
 }
-v_host() {
-    a2dissite 000-default.conf
-    rm -rf /etc/apache2/sites-available/000-default.conf
-cat << _EOF_ > /etc/apache2/sites-available/000-default.conf
-<VirtualHost *:80>
-    ServerName ${DOMAIN}
-    ServerAdmin webmaster@${DOMAIN}
-    DocumentRoot ${webroot}
-    ErrorLog ${APACHE_LOG_DIR}/${DOMAIN}_error.log
-    CustomLog ${APACHE_LOG_DIR}/${DOMAIN}access.log combined
 
-    <Directory "${webroot}">
+check()
+{
+    header
+    echo "Checking system for required components.."
+    sleep 2
+    which apache2 &>/dev/null
+        if [[ $? -ne 0 ]]; then
+            apt install apache2 -y &>/dev/null
+            systemctl enable apache2 &>/dev/null && systemctl start apache2 &>/dev/null
+        fi
+    command -v htpasswd &>/dev/null
+        if [[ $? -ne 0 ]]; then
+            echo "Installing required components.."
+            apt install apache2-utils -y &>/dev/null
+        fi
+        echo "Components installed.."
+        sleep 2
+}
+
+v_host() 
+{
+    a2dissite 000-default.conf &>/dev/null
+    cat <<- _EOF_ > /etc/apache2/sites-available/$DOMAIN.conf
+<VirtualHost *:80>
+    ServerName $DOMAIN
+    ServerAlias www.$DOMAIN
+    
+    ServerAdmin webmaster@${DOMAIN}
+    
+    DocumentRoot $WEBROOT
+    ErrorLog ${APACHE_LOG_DIR}/$DOMAIN_error.log
+    CustomLog ${APACHE_LOG_DIR}/$DOMAIN-access.log combined
+    <Directory "$WEBROOT">
         AuthType Basic
         AuthName "Restricted Content"
         AuthUserFile /etc/apache2/.htpasswd
@@ -48,31 +78,39 @@ cat << _EOF_ > /etc/apache2/sites-available/000-default.conf
     </Directory>
 </VirtualHost>
 _EOF_
-a2ensite 000-default.conf
-systemctl reload apache2
+a2ensite $DOMAIN.conf &>/dev/null
+systemctl reload apache2 &>/dev/null
 }
-if [[ $EUID -ne 0 ]]; then
-    clear
-    echo "****** ONLY ROOT CAN RUN THIS SCRIPT! ******"
-    sleep 3
-    exit 1
-fi
 
-check_installed
+# SCRIPT 
+########################
 
-gather_info
+checkroot
 
-htpasswd -b -c /etc/apache2/.htpasswd ${username} ${userpass}
+check
 
+echo "Generating .htpasswd file now.."
+sleep 2
+htpasswd -b -c /etc/apache2/.htpasswd $USERNAME $USERPASS &>/dev/null
+
+echo "Creating new virtual host file.."
+sleep 2
 v_host
 
-sed 's/AllowOverride None/AllowOverride All/g' /etc/apache2/apache2.conf
+echo "Modifying apache2.conf file.."
+sleep 2
+sed 's/AllowOverride None/AllowOverride All/g' /etc/apache2/apache2.conf &>/dev/null
 
-cat << _EOF_ > ${webroot}/.htaccess
+echo "Creating .htaccess file.."
+sleep 2
+cat << _EOF_ > $WEBROOT/.htaccess
 AuthType Basic
 AuthName "Restricted Content"
 AuthUserFile /etc/apache2/.htpasswd
 Require valid-user
 _EOF_
 
-systemctl reload apache2
+systemctl reload apache2 &>/dev/null
+
+echo "Script Complete"
+sleep 2
